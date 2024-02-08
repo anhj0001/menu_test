b08766d69012f77705738231fb6221e4a5d2ebb8
anhj0001 <anhj0001@stud.kea.dk>
2024-02-08 10:42:51 +0100

    stor ændring der virker

---
+++ produktliste.js
@@ -0,0 +1,40 @@
+const urlParams = new URLSearchParams(window.location.search);
+const category = urlParams.get("category");
+
+const url = "https://kea-alt-del.dk/t7/api/products?category=" + category;
+
+fetch(url)
+  .then((response) => response.json())
+  .then(dataReceived);
+
+function dataReceived(data) {
+  console.log(data);
+  data.forEach(listProducts);
+}
+
+function listProducts(product) {
+  console.log("listProducts");
+  const productlist = document.querySelector("template").content;
+
+  const myClone = productlist.cloneNode(true);
+
+  myClone.querySelector(".produktliste_tekst").textContent = product.productdisplayname;
+
+  myClone.querySelector(".category").textContent = product.season;
+
+  myClone.querySelector(".price .dkk").textContent = product.price;
+
+  myClone.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
+
+  // if (product.soldout) {
+  //   myClone.querySelector("div").classList.add("sold_out");
+  // }
+
+  myClone.querySelector(".link").setAttribute("href", "produkt.html?id=" + product.id);
+
+  document.querySelector(".grid_container").appendChild(myClone);
+
+  //myClone.querySelector(".link").setAttribute("href", `produkt.html?id=${product.id}`);
+  // Appende
+  // document.querySelector("main").appendChild(myClone);
+}
